396e8eea2517edfd4dcd00345944cf842f8e968c
Tony <com.bellavi@tutanota.com>
2020-11-24 23:49:40 +0100

    Fix bug on write

--- ft_write.s
+++ ft_write.s
@@ -1,11 +1,23 @@
 	section .bss
-	
+
 	global ft_write
 	extern __errno_location
 	section .text
 
 ft_write:
-	call		__errno_location wrt ..plt	; call errno
+	push		rbx
 	mov			rax, 1
 	syscall
+
+	cmp			rax, 0
+	jge			.end
+
+	neg			rax
+	mov			r8, rax
+	call		__errno_location wrt ..plt
+	mov			[rax], r8
+	mov			rax, -1
+
+.end:
+	pop			rbx
 	ret
