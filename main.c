f0ad2ea3f922ea7e88055805dbd386e3e0c8a04a
Tony <com.bellavi@tutanota.com>
2020-11-26 01:35:49 +0100

    Add test for strlen

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: bbellavi <bbellavi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 18:32:33 by bbellavi          #+#    #+#             */
-/*   Updated: 2020/11/26 00:49:02 by bbellavi         ###   ########.fr       */
+/*   Updated: 2020/11/26 01:34:48 by bbellavi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,17 @@ extern char	*ft_strdup(const char *s1);
 #define TEST_FILE2 "./test2"
 
 #define SIMPLE_STR "Hello World!\n"
+#define LONG_STR "Lorem ipsum dolor sit amet, consectetur adipiscing elit. \
+Duis interdum odio sit amet vulputate mollis. Proin vel ligula vel nisi \
+tristique dapibus ut eu leo. Aliquam leo mauris, maximus vitae dui at, \
+aliquam pharetra tellus. Nullam condimentum tincidunt aliquet. Quisque \
+id tortor pulvinar, fermentum turpis ut, vulputate ipsum. Donec sit amet \
+consequat dolor. Duis dictum magna augue, non efficitur purus pellentesque \
+scelerisque. Etiam dapibus lacus eu tristique vehicula. Cras quis massa vitae \
+quam cursus lacinia. Phasellus in tincidunt ligula. Etiam sit amet posuere \
+purus, eget rhoncus dui. Fusce nulla purus, congue interdum dolor vitae, \
+consectetur suscipit libero."
+
 #define SIMPLE_LEN strlen(SIMPLE_STR)
 #define EMPTY_STR ""
 
@@ -37,36 +48,44 @@ typedef struct {
 	int second;
 }		t_pair;
 
-#define Assert_int(expect, actual)								\
-{																\
-	if (actual != expect)										\
-		printf("Expected : %i but got : %i\n", expect, actual);	\
+#define Assert_int(expect, actual, test)													\
+{																							\
+	if (actual != expect)																	\
+		printf("Test: %s : Expected : %i but got : %i\n", test, expect, actual);			\
+	else																					\
+		printf("Test: %-30s : Success\n", test);											\
 }
 
-#define Assert_str(expect, actual)								\
-{																\
-	if (strcmp(expect, actual) != 0)							\
-		printf("Expected : %s but got : %s\n", expect, actual);	\
+#define Assert_str(expect, actual, test)													\
+{																							\
+	if (strcmp(expect, actual) != 0)														\
+		printf("Test: %s : Expected : %s but got : %s\n", test, expect, actual);			\
+	else																					\
+		printf("Test: %-30s : Success\n", test);											\
 }
 
-#define Assert_true(expr)											\
-{																	\
-	if (!(expr))													\
-		printf("Assertion failed, (%s) must be true\n", #expr);		\
+#define Assert_true(expr, test)																\
+{																							\
+	if (!(expr))																			\
+		printf("Test: %s : Assertion failed, (%s) must be true\n", test, #expr);			\
+	else																					\
+		printf("Test: %-30s : Success\n", test);											\
 }
 
-#define Assert_false(expr)											\
-{																	\
-	if (expr)														\
-		printf("Assertion failed, (%s) must be false\n", #expr);	\
+#define Assert_false(expr, test)															\
+{																							\
+	if (expr)																				\
+		printf("Test: %s : Assertion failed, (%s) must be false\n", test, #expr);			\
+	else																					\
+		printf("Test: %-30s : Success\n", test);											\
 }
 
-#define Test_Header(test)													\
-{																			\
-	printf("====================== %s ======================\n", test);		\
+#define Test_Header(test)																	\
+{																							\
+	printf("====================== %s ======================\n", test);						\
 }
 
-int	Test_write(void)
+void	Test_write(void)
 {
 	t_pair pair;
 	int actual;
@@ -77,47 +96,70 @@ int	Test_write(void)
 	// Write simple string to stdin
 	expect = write(STDIN_FILENO, SIMPLE_STR, SIMPLE_LEN);
 	actual = ft_write(STDIN_FILENO, SIMPLE_STR, SIMPLE_LEN);
-	Assert_int(expect, actual);
+	Assert_int(expect, actual, "simple string");
 
 	// Write empty string to stdin
 	expect = write(STDIN_FILENO, EMPTY_STR, 1);
 	actual = ft_write(STDIN_FILENO, EMPTY_STR, 1);
-	Assert_int(expect, actual);
+	Assert_int(expect, actual, "empty string");
 
 	// Write simple string into a file
 	pair.first = open(TEST_FILE1, O_RDWR);
 	if (pair.first == -1)
 	{
 		puts(strerror(errno));
-		return (errno);
+		return ;
 	}
 	pair.second = open(TEST_FILE2, O_RDWR);
 	if (pair.second == -1)
 	{
 		close(pair.first);
 		puts(strerror(errno));
-		return (errno);
+		return ;
 	}
 	expect = write(pair.first, SIMPLE_STR, SIMPLE_LEN);
 	actual = ft_write(pair.second, SIMPLE_STR, SIMPLE_LEN);
-	Assert_int(expect, actual);
+	Assert_int(expect, actual, "simple string to file");
 
 	expect = write(pair.first, EMPTY_STR, 0);
 	actual = ft_write(pair.second, EMPTY_STR, 0);
-	Assert_int(expect, actual);
+	Assert_int(expect, actual, "empty string to file");
 
 	close(pair.first);
 	close(pair.second);
 	actual = ft_write(pair.second, "", 0);
-	Assert_int(-1, actual);
-	Assert_true(actual == -1);
-	Assert_int(EBADF, errno);
-	Assert_true(errno == EBADF);
-	return (0);
+	Assert_int(-1, actual, "closed fd");
+	Assert_true(actual == -1, "closed fd");
+	Assert_int(EBADF, errno, "closed fd errno");
+	Assert_true(EBADF == errno, "closed fd errno");
+}
+
+void	Test_strlen(void)
+{
+	int actual;
+	int expect;
+
+	Test_Header("Strlen");
+
+	// Basic test
+	expect = strlen(SIMPLE_STR);
+	actual = ft_strlen(SIMPLE_STR);
+	Assert_int(expect, actual, "simple string");
+
+	// Test with empty string
+	expect = strlen(EMPTY_STR);
+	actual = ft_strlen(EMPTY_STR);
+	Assert_int(expect, actual, "empty string");
+
+	// Test with long string
+	expect = strlen(LONG_STR);
+	actual = ft_strlen(LONG_STR);
+	Assert_int(expect, actual, "long string");
 }
 
 int	main(void)
 {
 	Test_write();
+	Test_strlen();
 	return (0);
 }
